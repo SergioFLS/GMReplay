4978f92570030cb04cfbc2742e66eb152f0ac59a
OceanBagel <50093445+OceanBagel@users.noreply.github.com>
2024-12-18 00:09:40 -0600

    v0.0.5
    Abstracted out the function replacement and added a patch to replace randomize() with random_get_seed() to make RNG deterministic

--- gmreplay.py
+++ gmreplay.py
@@ -4,7 +4,7 @@ import os
 import re
 
 
-VERSION_NUMBER = "v0.0.4"
+VERSION_NUMBER = "v0.0.5"
 
 
 def main():
@@ -105,6 +105,8 @@ def browseFile(promptText, fileExists, extensions=[("All files","*.*"), ("GMRepl
 def genPatchedExe(exePath, patchedName="__temp_GMR_patched_runner.exe"):
     ## Apply patches to the chosen exe and save it into the current working directory. Returns a path to the patched exe.
 
+    exeModified = False
+
     # Read the exe into memory
     with open(exePath, 'rb') as fid:
         exeData = fid.read()
@@ -131,49 +133,73 @@ def genPatchedExe(exePath, patchedName="__temp_GMR_patched_runner.exe"):
             for match_obj in mouseRegex.finditer(exeData):
                 mouseOffsets += [match_obj.start()]
 
-        if len(mouseOffsets) != 2: # if it's still not 2 after we ran the second check
-            print("WARNING: Patching was unsuccessful. Mouse inputs and direct keyboard inputs will not work.\n"+\
-                  "Falling back on the unpatched exe file...")
-            return exePath
+    if len(mouseOffsets) != 2: # if it's still not 2 after we ran the second check
+        print("WARNING: Patching encountered an issue. Mouse inputs will not work.")
 
-    # If there are exactly two matches, we have g_MouseX and g_MouseY offsets and values
-    g_MouseX_offsets = 2*[0]
-    g_MouseY_offsets = 2*[0]
+    else: # Keep going if there is no issue, otherwise skip ahead to the next patch
+        # If there are exactly two matches, we have g_MouseX and g_MouseY offsets and values
+        g_MouseX_offsets = 2*[0]
+        g_MouseY_offsets = 2*[0]
 
-    g_MouseX_offsets[0] = mouseOffsets[0] + 5
-    g_MouseX_offsets[1] = mouseOffsets[1] + 5
+        g_MouseX_offsets[0] = mouseOffsets[0] + 5
+        g_MouseX_offsets[1] = mouseOffsets[1] + 5
 
-    g_MouseY_offsets[0] = mouseOffsets[0] + 28
-    g_MouseY_offsets[1] = mouseOffsets[1] + 28 + patternOffset
+        g_MouseY_offsets[0] = mouseOffsets[0] + 28
+        g_MouseY_offsets[1] = mouseOffsets[1] + 28 + patternOffset
 
-    g_MouseX = exeData[g_MouseX_offsets[0]:g_MouseX_offsets[0] + 4]
-    g_MouseY = exeData[g_MouseY_offsets[0]:g_MouseY_offsets[0] + 4]
+        g_MouseX = exeData[g_MouseX_offsets[0]:g_MouseX_offsets[0] + 4]
+        g_MouseY = exeData[g_MouseY_offsets[0]:g_MouseY_offsets[0] + 4]
 
-    # Next we search forward past g_MouseY_offsets[1] to find the last pattern for g_MouseX
-    mouseXUpdateOffset = exeData.find(g_MouseX, max(g_MouseY_offsets[0], g_MouseY_offsets[1]))
-    mouseYUpdateOffset = exeData.find(g_MouseY, mouseXUpdateOffset-64) # it could also be before mouseX on older versions
+        # Next we search forward past g_MouseY_offsets[1] to find the last pattern for g_MouseX
+        mouseXUpdateOffset = exeData.find(g_MouseX, max(g_MouseY_offsets[0], g_MouseY_offsets[1]))
+        mouseYUpdateOffset = exeData.find(g_MouseY, mouseXUpdateOffset-64) # it could also be before mouseX on older versions
 
-    # Take the last bytes of g_MouseX/Y for search terms
-    g_MousePosX_offset = exeData.rfind(g_MouseX[-1], mouseXUpdateOffset - 8, mouseXUpdateOffset) - 3
-    g_MousePosY_offset = exeData.rfind(g_MouseY[-1], mouseYUpdateOffset - 8, mouseYUpdateOffset) - 3
+        # Take the last bytes of g_MouseX/Y for search terms
+        g_MousePosX_offset = exeData.rfind(g_MouseX[-1], mouseXUpdateOffset - 8, mouseXUpdateOffset) - 3
+        g_MousePosY_offset = exeData.rfind(g_MouseY[-1], mouseYUpdateOffset - 8, mouseYUpdateOffset) - 3
 
-    # We now have g_MousePosX/Y
-    g_MousePosX = exeData[g_MousePosX_offset:g_MousePosX_offset + 4]
-    g_MousePosY = exeData[g_MousePosY_offset:g_MousePosY_offset + 4]
+        # We now have g_MousePosX/Y
+        g_MousePosX = exeData[g_MousePosX_offset:g_MousePosX_offset + 4]
+        g_MousePosY = exeData[g_MousePosY_offset:g_MousePosY_offset + 4]
 
-    # Finally, we overwrite g_MouseX/Y with g_MousePosX/Y
-    newExeData[g_MouseX_offsets[0]:g_MouseX_offsets[0] + 4] = g_MousePosX
-    newExeData[g_MouseX_offsets[1]:g_MouseX_offsets[1] + 4] = g_MousePosX
-    newExeData[g_MouseY_offsets[0]:g_MouseY_offsets[0] + 4] = g_MousePosY
-    newExeData[g_MouseY_offsets[1]:g_MouseY_offsets[1] + 4] = g_MousePosY
+        # Finally, we overwrite g_MouseX/Y with g_MousePosX/Y
+        newExeData[g_MouseX_offsets[0]:g_MouseX_offsets[0] + 4] = g_MousePosX
+        newExeData[g_MouseX_offsets[1]:g_MouseX_offsets[1] + 4] = g_MousePosX
+        newExeData[g_MouseY_offsets[0]:g_MouseY_offsets[0] + 4] = g_MousePosY
+        newExeData[g_MouseY_offsets[1]:g_MouseY_offsets[1] + 4] = g_MousePosY
+        exeModified = True
 
 
     # Patch keyboard_check_direct next
-    kcdStringOffset = exeData.find(b"keyboard_check_direct\x00")
-    kcStringOffset = exeData.find(b"keyboard_check\x00")
+    newExeData = replaceFunction(exeData, "keyboard_check", "keyboard_check_direct", newExeData,\
+                                 "WARNING: Patching encountered an issue. Direct keyboard input will not work.")
 
-    kcdFuncDefOffset = -1
-    kcFuncDefOffset = -1
+
+    # Patch randomize
+    newExeData = replaceFunction(exeData, "random_get_seed", "randomize", newExeData,\
+                                 "WARNING: Patching encountered an issue. Randomness will not be deterministic.")
+
+
+    # Done with the patched exe, now write it to the working directory
+    if exeModified == True:
+        with open(patchedName, 'wb') as fid:
+            fid.write(newExeData)
+        return patchedName
+
+    else:
+        return exePath
+
+def replaceFunction(exeData, sourceFuncName, destFuncName, newExeData, failureMessage):
+    ## Replaces one function by overwriting its definition with another function
+
+    global exeModified # for indicating to the calling function whether the patch was successful
+
+    # Find the offsets to the string names
+    destStringOffset = exeData.find(bytes(destFuncName, "utf-8") + b"\x00")
+    sourceStringOffset = exeData.find(bytes(sourceFuncName, "utf-8") + b"\x00")
+
+    destFuncDefOffset = -1
+    sourceFuncDefOffset = -1
 
     # Find the magic word representing the start of the image optional header
     imgOptHeadrAddr = exeData.find(b"\x0b\x01", 0x0, 0x200) # It should be at the beginning, otherwise oopsies
@@ -195,29 +221,21 @@ def genPatchedExe(exePath, patchedName="__temp_GMR_patched_runner.exe"):
         dataPtrOffset = dataBaseDiff + imageBase
 
         # Now we have what we need to find the function definitions
-        kcdFuncDefOffset = exeData.find(b"\x68" + intToBytes(kcdStringOffset + dataPtrOffset)[::-1])
-        kcFuncDefOffset = exeData.find(b"\x68" + intToBytes(kcStringOffset + dataPtrOffset)[::-1])
+        destFuncDefOffset = exeData.find(b"\x68" + intToBytes(destStringOffset + dataPtrOffset)[::-1])
+        sourceFuncDefOffset = exeData.find(b"\x68" + intToBytes(sourceStringOffset + dataPtrOffset)[::-1])
 
-    else: # We couldn't do the patch
-        print("WARNING: Patching was partially successful. Mouse inputs will work, but direct keyboard input will not.")
-
-        # Patch the mouse changes
-        with open(patchedName, 'wb') as fid:
-            fid.write(newExeData)
-        return patchedName
+    if imgOptHeadrAddr == -1 or destFuncDefOffset == -1 or sourceFuncDefOffset == -1: # We couldn't do the patch
+        print(failureMessage)
+        return newExeData
 
     # The function itself starts 4 bytes earlier
-    kcFunction = exeData[kcFuncDefOffset-4:kcFuncDefOffset]
+    sourceFunction = exeData[sourceFuncDefOffset-4:sourceFuncDefOffset]
 
     # Overwrite the function
-    newExeData[kcdFuncDefOffset-4:kcdFuncDefOffset] = kcFunction
+    newExeData[destFuncDefOffset-4:destFuncDefOffset] = sourceFunction
+    exeModified = True
 
-
-    # Done with the patched exe, now write it to the working directory
-    with open(patchedName, 'wb') as fid:
-        fid.write(newExeData)
-
-    return patchedName
+    return newExeData
 
 
 if __name__ == '__main__':
